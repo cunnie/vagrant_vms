3c127823c6442f3cbf3eda78691898b3f750f89a
Brian Cunnie <brian.cunnie@gmail.com>
2014-07-06 15:14:49 -0700

    FreeBSD needs gateway set and NTP started

---
+++ fbsd_10.0/gateway_and_ntp.sh
@@ -0,0 +1,6 @@
+  #!/usr/bin/env bash
+  route delete default 10.0.2.2
+  route add default 10.9.9.1
+  apt-get install -y ntp
+  grep ntpd_enable /etc/rc.conf || echo 'ntpd_enable="YES"' >> /etc/rc.conf
+  /etc/rc.d/ntpd start
